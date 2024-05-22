167adcb251cb8a6deaff0d9c6a90dfcf9b96ade5
Vitor Baptista <166644881+vitorbap@users.noreply.github.com>
2024-05-22 09:48:57 -0300

    Add files via upload

--- Program.cs
+++ Program.cs
@@ -1,3 +1,13 @@
 ﻿using DesafioPOO.Models;
 
 // TODO: Realizar os testes com as classes Nokia e Iphone
+
+Iphone iph = new Iphone("(15)99999-9999","Iphone 15","26354646543243",512);
+iph.Ligar();
+iph.InstalarAplicativo("Instagram");
+iph.ReceberLigacao();
+Console.WriteLine("");
+Nokia nk =new Nokia("(15)99999-9999","Nokia G60 5G","1658768542316845314",512);
+nk.Ligar();
+nk.InstalarAplicativo("Shopee");
+nk.ReceberLigacao();
